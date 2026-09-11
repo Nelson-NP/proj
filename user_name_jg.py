fae31dd3230e9191655456a8c1eed5528711ff3d
Nelson NP <nelsonnoen45@gmail.com>
2026-09-11 17:43:52 +0100

    Version 07

--- user_name_jg.py
+++ user_name_jg.py
@@ -1,6 +1,6 @@
 
 nome = str(input('Digite o seu nome? '))
 
-print(f'Olá {nome}, como estás?\n')
-
-print('Será sempre um prazer ajudar vĉce')
+if nome = 'Nelson':
+    print(f'Oi, {nome} que nome lido!')
+print('Boa noite.')
